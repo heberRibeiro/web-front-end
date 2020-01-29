99ea1d8c89d801cd392bb58ee3465c02f5ef6287
Heber Ribeiro <heber.r@outlook.com>
2020-01-29 13:02:20 -0300

    Página de apresentação

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,9 +1,14 @@
 (function () {
+    const navBar = document.querySelector('[hiddenVisible]')
+
     function navegarViaAjax(hash) {
         if (!hash) return
 
-        //const link = document.querySelector(`[href-link='${hash}']`)
-       // if(!link) return
+        // if (hash == '#/paginas/bemVindo') {
+        //     document.querySelector('[hiddenVisible]').style.visibility = 'hidden'
+        // } else {
+        //     document.querySelector('[hiddenVisible]').style.visibility = 'visible'
+        // }
 
         const destino = document.querySelector('[href-link-destino]')
 
@@ -15,24 +20,38 @@
             })
     }
 
-    function configurarLinks() {
-        document.querySelectorAll('[href-link]')
-            .forEach(link => {
-                link.href = link.attributes['href-link'].value
-            })
-    }
-
     function navegacaoInicial() {
         if (location.hash) {
             navegarViaAjax(location.hash)
         } else {
-           // const primeiroLink = document.querySelector('[href-link]')
             navegarViaAjax('#/paginas/bemVindo')
+        }
+    }
+
+   
+    navBar.onmouseenter = e => {
+        e.preventDefault()
+        if (location.hash == '' || '#/paginas/bemVindo') {
+            console.log('entrou')
+        //     const html = document.getElementsByTagName('html')
+        //     console.log(...Array.from(html))
+        //    Array.from(html)[0].style.top = '0.0rem'
+        //    navBar.style.opacity = '1.0'
+        navBar.classList.toggle("myToggle")
+           
+           
+            
+        }
+    }
+
+    navBar.onmouseleave = e => {
+        e.preventDefault()
+        if (location.hash == '' || '#/paginas/bemVindo') {
+            console.log('saiu')
+            
         }
     }
 
     navegacaoInicial()
     window.onhashchange = e => navegarViaAjax(location.hash)
-    
-    configurarLinks()
 })()
