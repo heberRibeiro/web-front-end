d6082dbb79307baaf1c94ee677998b2f73ed0c5b
Heber Ribeiro <heber.r@outlook.com>
2020-01-29 15:25:12 -0300

    Página cageTheElephant

--- js/navegacao.js
+++ js/navegacao.js
@@ -4,12 +4,6 @@
     function navegarViaAjax(hash) {
         if (!hash) return
 
-        // if (hash == '#/paginas/bemVindo') {
-        //     document.querySelector('[hiddenVisible]').style.visibility = 'hidden'
-        // } else {
-        //     document.querySelector('[hiddenVisible]').style.visibility = 'visible'
-        // }
-
         const destino = document.querySelector('[href-link-destino]')
 
         const url = hash.substring(1)
@@ -25,30 +19,6 @@
             navegarViaAjax(location.hash)
         } else {
             navegarViaAjax('#/paginas/bemVindo')
-        }
-    }
-
-   
-    navBar.onmouseenter = e => {
-        e.preventDefault()
-        if (location.hash == '' || '#/paginas/bemVindo') {
-            console.log('entrou')
-        //     const html = document.getElementsByTagName('html')
-        //     console.log(...Array.from(html))
-        //    Array.from(html)[0].style.top = '0.0rem'
-        //    navBar.style.opacity = '1.0'
-        navBar.classList.toggle("myToggle")
-           
-           
-            
-        }
-    }
-
-    navBar.onmouseleave = e => {
-        e.preventDefault()
-        if (location.hash == '' || '#/paginas/bemVindo') {
-            console.log('saiu')
-            
         }
     }
 
